4e4a700b3ab78fc437c18188f812a41b04e9e0c0
akakou <akakou571@gmail.com>
2023-04-24 20:51:40 +0900

    [update] use common id tags

---
+++ demo/js/tags.js
@@ -0,0 +1,12 @@
+
+const cipher = document.getElementById('cipher')
+const plain = document.getElementById('plain')
+const pubkey = document.getElementById('pubkey')
+const privkey = document.getElementById('privkey')
+
+const tags = {
+    cipher,
+    plain,
+    pubkey,
+    privkey
+}
